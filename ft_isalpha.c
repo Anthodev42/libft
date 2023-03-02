abe4e4b165434f344c2a521426cbf850d08c3186
anthony <anthonydev42@gmail.com>
2023-03-02 17:50:47 +0100

    ft_isaplpha

---
+++ ft_isalpha.c
@@ -0,0 +1,14 @@
+#include "libft.h"
+
+int ft_isalpha(int character)
+{
+	if ((character >= 'A' && character <= 'Z') || (character >= 'a' && character <= 'z')) 
+	{
+		return (1024);
+	}
+
+	else 
+	{
+		return (0);
+	}
+}
